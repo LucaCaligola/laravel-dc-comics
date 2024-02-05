b2d31691cfc2cc5b65868f9c7543ebb72589a40a
LucaCaligola <luca.caligola@gmail.com>
2024-02-05 17:52:26 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<section class="header bg-white">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12 d-flex align-items-center justify-content-center py-2">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="" class="logo">
+                <div class="lista">
+                    <ul class="d-flex list-unstyled">
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
